cca90eaf72fc7a12e971769a349c2085770bcf78
Priyanshu <priyanshu.gupta22b@iiitg.ac.in>
2025-07-19 08:54:20 +0530

    WIP

---
+++ src/constant/__init__.py
@@ -0,0 +1,11 @@
+import os
+
+AWS_S3_BUCKET_NAME = "wafer-fault"
+MONGO_DATABASE_NAME = "DS"
+MONGO_COLLECTION_NAME = "waferfalut"
+
+TARGET_COLUMN = "quality"
+MONGO_DB_URL = ""
+
+MONGO_FILE_NAME = ".model"
+MONGO_FILE_EXTENSION =".pkl"
